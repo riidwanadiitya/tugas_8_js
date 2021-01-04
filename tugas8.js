c4e8b133dd182e6b6decfebd40e09aea64494c53
Ridwan <71011389+riidwanadiitya@users.noreply.github.com>
2021-01-04 08:55:52 +0700

    TUGAS 8 JS

---
+++ tugas8.js
@@ -0,0 +1,11 @@
+function dataSiswa() {
+    var dataPribadi = {
+        nama: "Ridwan Aditya Saputra",
+        tahunLahir: 1996,
+        tahunAjaran: 2020,
+    }
+    dataPribadi.alamat = "Bogor";
+    dataPribadi.tahunAjaran = 2011;
+    console.log(dataPribadi);
+}
+dataSiswa()
